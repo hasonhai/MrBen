96093f32b961abcae184f63cc17ab93013c9f07e
Ha Son Hai <hasonhai124@gmail.com>
2014-08-23 16:27:45 +0200

    fix bugs

--- disk/test.sh
+++ disk/test.sh
@@ -7,22 +7,28 @@ USER="ubuntu"
 CONF="testdir/four-threads-randio.fio"
 
 chmod a+x disktest.sh
+echo "Setup the host"
 for HOST in $HOSTLIST; do
     ./disktest.sh $CONF $USER $KEY $HOST setup-host
 done
 
+echo "Setup the test"
 for HOST in $HOSTLIST; do
+    echo "Setup the test"
     ./disktest.sh $CONF $USER $KEY $HOST setup-test
 done
 
+echo "Running the test"
 for HOST in $HOSTLIST; do
     ./disktest.sh $CONF $USER $KEY $HOST run
 done
 
+echo "Collecting test output"
 for HOST in $HOSTLIST; do
     ./disktest.sh $CONF $USER $KEY $HOST collect-data
 done
 
+echo "Clean the host"
 for HOST in $HOSTLIST; do
     ./disktest.sh $CONF $USER $KEY $HOST clean
 done
